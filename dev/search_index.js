aa62d5c895bead483cac8b0e9d8c7f7fb12e64b4
Documenter.jl <documenter@juliadocs.github.io>
2023-06-07 00:35:37 +0000

    build based on 03e60f4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RandomLogos","category":"page"},{"location":"#RandomLogos","page":"Home","title":"RandomLogos","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RandomLogos.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RandomLogos]","category":"page"}]
+}
