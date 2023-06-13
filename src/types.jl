b6a8c81e039ac0b7611ba77ab8a5462ba4d3317e
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2023-06-13 21:44:42 +0900

    add docstring for Affine

--- src/types.jl
+++ src/types.jl
@@ -1,5 +1,21 @@
 """
 Type for an affine transformation
+
+``
+\\textrm{Affine}(x) = Wx + b
+``
+
+# Example
+
+```julia-repl
+julia> using RandomLogos: Affine
+julia> using StaticArrays
+julia> W = @SMatrix rand(2, 2)
+julia> b = @SVector rand(2)
+julia> aff = Affine(W, b)
+julia> x = @SVector rand(2)
+julia> aff(x)
+```
 """
 @concrete struct Affine
     W
